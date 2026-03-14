bcc1edf0e01452dc70ae5adf9bbec4a53af8aef7
Damien Bataillard <damiebtd@gmail.com>
2026-03-14 12:01:15 +0100

    Suggestions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 function App() {
   const [query, setQuery] = useState("");
   const [cards, setCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const isSelecting = useRef(false);
 
   const searchCards = async () => {
     try {
@@ -26,40 +30,96 @@ function App() {
   };
 
   useEffect(() => {
+    if (isSelecting.current) {
+      isSelecting.current = false;
+      return;
+    }
+
     if (query.trim().length < 2) {
       setCards([]);
+      setShowSuggestions(false);
       return;
     }
 
     const timeout = setTimeout(() => {
-      searchCards(query);
+      searchCards();
+      setShowSuggestions(true);
     }, 400);
 
     return () => clearTimeout(timeout);
-  }, [query]);
+  }, [query]);;
+
+  const handleSelectCard = (card) => {
+    isSelecting.current = true;
+    setSelectedCard(card);
+    setQuery(card.name);
+    setShowSuggestions(false);
+  };
 
   return (
     <div className="page">
-      <h1 className="title">Research a yu gi oh card</h1>
+      <h1 className="title">Search a card</h1>
 
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Ex: Blue-Eyes White Dragon"/>
+      <div className="search-container">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+            setSelectedCard(null);
+          }}
+          onFocus={() => {
+            if (cards.length > 0) {
+              setShowSuggestions(true);
+            }
+          }}
+          placeholder="Ex: Blue-Eyes White Dragon"
+          className="search-input"
+        />
 
-      {loading && <p>Loading...</p>}
+        {loading && <p>Loading...</p>}
 
-      <div className="result">
-        {cards.map((card) => (
-          <div className="slot">
-            {card.card_images?.[0]?.image_url && (
-                  <img src={card.card_images[0].image_url} alt={card.name} className="imagecard"/>
+        {showSuggestions && cards.length > 0 && (
+          <div className="suggestions">
+            {cards.slice(0, 8).map((card) => (
+              <div
+                key={card.id}
+                className="suggestion-item"
+                onClick={() => handleSelectCard(card)}
+              >
+                {card.card_images?.[0]?.image_url && (
+                  <img
+                    src={card.card_images[0].image_url}
+                    alt={card.name}
+                    className="suggestion-image"
+                  />
                 )}
-            <div className="card">
-              <h2 className="cardname">{card.name}</h2>
-                <p>{card.type} {card.race} {card.attribute} {card.level} {card.atk} {card.def} </p>
-                <p>{card.desc}</p>
-            </div>
+
+                <div className="suggestion-text">
+                  <strong>{card.name}</strong>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        )}
       </div>
+
+      {selectedCard && (
+        <div className="card">
+          {selectedCard.card_images?.[0]?.image_url && (
+            <img
+              src={selectedCard.card_images[0].image_url}
+              alt={selectedCard.name}
+              className="imagecard"
+            />
+          )}
+          <div className="cardinfo">
+            <h2 className="cardname">{selectedCard.name}</h2>
+            <p>{selectedCard.type} {selectedCard.race} {selectedCard.atk} {selectedCard.def}</p>
+            <p>{selectedCard.desc}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
