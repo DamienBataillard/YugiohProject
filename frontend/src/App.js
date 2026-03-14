0d5f9719765dc938c7769cbe95e7e73b585cac6a
Damien Bataillard <damiebtd@gmail.com>
2026-03-14 11:10:29 +0100

    Création du style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
@@ -14,6 +14,8 @@ function App() {
         params: { name: query },
       });
 
+      console.log(res)
+
       setCards(res.data.data || []);
     } catch (error) {
       console.error(error);
@@ -23,41 +25,38 @@ function App() {
     }
   };
 
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Research a yu gi oh card</h1>
+  useEffect(() => {
+    if (query.trim().length < 2) {
+      setCards([]);
+      return;
+    }
 
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Ex: Blue-Eyes White Dragon"
-      />
-      <button onClick={searchCards}>Search</button>
+    const timeout = setTimeout(() => {
+      searchCards(query);
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [query]);
+
+  return (
+    <div className="page">
+      <h1 className="title">Research a yu gi oh card</h1>
+
+      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Ex: Blue-Eyes White Dragon"/>
 
       {loading && <p>Loading...</p>}
 
-      <div style={{ marginTop: "20px" }}>
+      <div className="result">
         {cards.map((card) => (
-          <div
-            key={card.id}
-            style={{
-              border: "1px solid #ccc",
-              padding: "10px",
-              marginBottom: "15px",
-            }}
-          >
-            <h2>{card.name}</h2>
-            <p>{card.type}</p>
-            <p>{card.desc}</p>
-
+          <div className="slot">
             {card.card_images?.[0]?.image_url && (
-              <img
-                src={card.card_images[0].image_url}
-                alt={card.name}
-                width="200"
-              />
-            )}
+                  <img src={card.card_images[0].image_url} alt={card.name} className="imagecard"/>
+                )}
+            <div className="card">
+              <h2 className="cardname">{card.name}</h2>
+                <p>{card.type} {card.race} {card.attribute} {card.level} {card.atk} {card.def} </p>
+                <p>{card.desc}</p>
+            </div>
           </div>
         ))}
       </div>
