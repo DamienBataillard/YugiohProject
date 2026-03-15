ca79c6e3a93e652b04a8ff7c1567a81eed89753c
Damien Bataillard <damiebtd@gmail.com>
2026-03-15 10:34:54 +0100

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SearchCardPage from "./pages/SearchCardsPage";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: "20px", display: "flex", gap: "20px" }}>
-        <Link to="/">Home</Link>
-        <Link to="/search-card">Search card</Link>
-      </nav>
-
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search-card" element={<SearchCardPage />} />
